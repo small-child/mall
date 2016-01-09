74d90d3f4c262c6d153cf65c6d3703d7736832a2
small-child <136596826@qq.com>
2016-01-09 21:57:35 +0800

    contract over

--- public/js/contract_text.js
+++ public/js/contract_text.js
@@ -4,7 +4,39 @@ $(function() {
     /*print*/
     $('#wx-print').click(function () {
         $("#print").jqprint();
-    })    
+    });
+
+    var digitUppercase = function(n) {  
+        var fraction = ['角', '分'];  
+        var digit = [  
+            '零', '壹', '贰', '叁', '肆',  
+            '伍', '陆', '柒', '捌', '玖'  
+        ];  
+        var unit = [  
+            ['元', '万', '亿'],  
+            ['', '拾', '佰', '仟']  
+        ];  
+        var head = n < 0 ? '欠' : '';  
+        n = Math.abs(n);  
+        var s = '';  
+        for (var i = 0; i < fraction.length; i++) {  
+            s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');  
+        }  
+        s = s || '整';  
+        n = Math.floor(n);  
+        for (var i = 0; i < unit[0].length && n > 0; i++) {  
+            var p = '';  
+            for (var j = 0; j < unit[1].length && n > 0; j++) {  
+                p = digit[n % 10] + unit[1][j] + p;  
+                n = Math.floor(n / 10);  
+            }  
+            s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;  
+        }  
+        return head + s.replace(/(零.)*零元/, '元')  
+            .replace(/(零.)+/g, '零')  
+            .replace(/^整$/, '零元整');  
+    };  
+
 	Date.prototype.Format = function(fmt)   
     { //author: meizz   
       var o = {   
@@ -39,7 +71,8 @@ $(function() {
         // console.log(data[0]);
         $('#wx-6').text(data[0].volume1);  						//数量
         $('#wx-7').text(data[0].unit_price1);  					//单价
-        $('#wx-8').text(data[0].volume1*data[0].unit_price1);  //金额      
+        $('#wx-8').text(data[0].volume1*data[0].unit_price1);   //金额  
+        $('#wx-10').text(digitUppercase(data[0].volume1*data[0].unit_price1));  //大写金额        
     })
 
     /*页面加载时，获取到信息*/
